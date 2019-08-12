ea891d1767c6ce12ce4520e3e53159c6737cc47d
Apin <apin.klas@gmail.com>
2019-08-12 08:06:16 +0700

    progress feature lang

--- src/react/action.js
+++ src/react/action.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Row, Col, Button, Popconfirm } from 'antd'
+import { Tooltip, Button, Popconfirm, Dropdown, Menu, Icon } from 'antd'
 import { GlobalStore } from './store'
 
 const Action = (props) => {
@@ -17,41 +17,66 @@ const Action = (props) => {
         dispatch({ type: 'removeGroup' })
     }
 
-    const onImport = () => {
-        dispatch({ type: 'import' })
-    }
-
-    const onExport = () => {
-        dispatch({ type: 'export' })
-    }
-
-    const onGenerate = () => {
-        dispatch({ type: 'generate' });
-    }
-
     const onReload = () => {
         dispatch({ type: 'reload' });
     }
 
-    return <Row>
-        <Col span={24}>
-            <Button.Group>
-                <Button type="default" icon="plus" onClick={onAddGroup}>Add group</Button>
-                {globalState.groups.length > 1 &&
-                    <Popconfirm title="Sure to delete?" onConfirm={onRemoveGroup}>
-                        <Button type="danger" icon="delete">Remove group</Button>
-                    </Popconfirm>
-                }
-            </Button.Group>
-            <Button style={{ marginLeft: 10 }} onClick={onAddRow} icon="plus" type="default">Add key</Button>
-            <Button.Group style={{ marginLeft: 10 }}>
-                <Button onClick={onImport} icon="upload" type="default">Import excel</Button>
-                <Button onClick={onExport} icon="download" type="default">Export excel</Button>
-            </Button.Group>
-            <Button style={{ marginLeft: 10 }} onClick={onGenerate} icon="exclamation" type="default">Generate dart code</Button>
-            <Button style={{ marginLeft: 10 }} onClick={onReload} icon="reload" type="default">Reload</Button>
-        </Col>
-    </Row>
+    const handleMenuClick = (e) => {
+        switch (e.key) {
+            case 'import':
+                dispatch({ type: 'import' })
+                break;
+            case 'export':
+                dispatch({ type: 'export' })
+                break;
+            case 'generate':
+                dispatch({ type: 'generate' });
+                break;
+        }
+    }
+
+    const menu = (
+        <Menu onClick={handleMenuClick}>
+            <Menu.Item key="manage">
+                <Icon type="unordered-list" />
+                Manage language
+          </Menu.Item>
+            <Menu.Divider />
+            <Menu.Item key="import">
+                <Icon type="upload" />
+                Import excel
+          </Menu.Item>
+            <Menu.Item key="export">
+                <Icon type="download" />
+                Export excel
+          </Menu.Item>
+            <Menu.Divider />
+            <Menu.Item key="generate">
+                <Icon type="exclamation" />
+                Generate dart code
+          </Menu.Item>
+        </Menu>
+    );
+
+    return <div>
+        <Button.Group>
+            <Button type="default" icon="plus" onClick={onAddGroup}>Add group</Button>
+            {globalState.groups.length > 1 &&
+                <Popconfirm title="Sure to delete?" onConfirm={onRemoveGroup}>
+                    <Button type="danger" icon="delete">Remove group</Button>
+                </Popconfirm>
+            }
+        </Button.Group>
+        <Button style={{ marginLeft: 10 }} onClick={onAddRow} icon="plus" type="default">Add key</Button>
+        <Button.Group style={{ marginLeft: 10 }}>
+            <Tooltip title="refresh" >
+                <Button onClick={onReload} icon="reload" type="default"></Button>
+            </Tooltip>
+            <Dropdown overlay={menu}>
+                <Button icon="more" type="default"></Button>
+            </Dropdown>
+        </Button.Group>
+    </div>
 }
 
 export default Action;
